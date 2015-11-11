a529520a67bbd75e082590cf1eb53d717b59af10
Felipe Pierin <felipe.pierin@gmail.com>
2015-11-11 10:01:14 -0200

    execucao multithread

--- src/main/java/br/usp/ime/wemantic/client/usp/UspEventService.java
+++ src/main/java/br/usp/ime/wemantic/client/usp/UspEventService.java
@@ -1,14 +1,25 @@
 package br.usp.ime.wemantic.client.usp;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringEscapeUtils;
 
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+
 import br.usp.ime.wemantic.commons.HttpResponse;
 import br.usp.ime.wemantic.commons.RestfullHttpClient;
+import br.usp.ime.wemantic.commons.RestfullHttpClientImpl;
 import br.usp.ime.wemantic.model.Event;
 
 public class UspEventService implements EventService {
@@ -21,48 +32,85 @@ public class UspEventService implements EventService {
 
 	@Override
 	public List<Event> retrieve() {
-		final String html = retrieveHtml();
-		return eventsInProgress(html);
+		final String uri = "http://www.eventos.usp.br/";
+		final String html = extractHtmlFromURI(uri);
+		final Set<String> eventsUris = eventsUrisFromHtml(html);
+
+		final ExecutorService executor = Executors.newFixedThreadPool(50);
+		final ListeningExecutorService les = MoreExecutors.listeningDecorator(executor);
+
+		final List<Callable<Event>> eventCalls = createEventCalls(eventsUris);
+		final List<Event> resultsFuture = new ArrayList<>();
+		try {
+			final List<Future<Event>> invokeAll = les.invokeAll(eventCalls);
+			for (final Future<Event> future : invokeAll) {
+				try {
+					resultsFuture.add(future.get());
+				} catch (final ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+		} catch (final InterruptedException e1) {
+			e1.printStackTrace();
+		}
+
+		executor.shutdown();
+		return resultsFuture;
 	}
 
-	private String retrieveHtml() {
-		final HttpResponse httpResponse = restfullHttpClient.get("http://www.eventos.usp.br/");
+	private List<Callable<Event>> createEventCalls(final Set<String> eventsUris) {
+		final List<Callable<Event>> cList = new ArrayList<>();
+		for (final String eventUri : eventsUris) {
+			cList.add(new Callable<Event>() {
+				@Override
+				public Event call() throws Exception {
+					return eventFromURI(eventUri);
+				}
+
+			});
+		}
+		return cList;
+	}
+
+	private String extractHtmlFromURI(final String uri) {
+		final HttpResponse httpResponse = restfullHttpClient.get(uri);
 		final String message = httpResponse.getMessage();
 		return message;
 	}
 
-	private List<Event> eventsInProgress(final String html) {
-		final String eventInProgressPattern = "<ul id=\"list-ev-anda\" class=\"front-eve\">(.*?)</ul>";
-		final Pattern pattern = Pattern.compile(eventInProgressPattern, Pattern.DOTALL);
+	private Set<String> eventsUrisFromHtml(final String html) {
+		final Set<String> eventUris = new HashSet<String>();
+		final String eventInProgressPattern = "href=\"(http://www.eventos.usp.br/\\?events=(.*?))\"";
+		final Pattern pattern = Pattern.compile(eventInProgressPattern);
 		final Matcher matcher = pattern.matcher(html);
 		while (matcher.find()) {
 			final String node = matcher.group(1);
-			return eventsFrom(node);
+			eventUris.add(node);
+		}
+		return eventUris;
+	}
+
+	private Event eventFromURI(final String uri) {
+		final String eventHtml = extractHtmlFromURI(uri);
+		final String titlePattern = "<div class=\"evento-titulo\" style=\"width: 470px;\">(.*?)</div>";
+		final Pattern pattern = Pattern.compile(titlePattern, Pattern.DOTALL);
+		final Matcher matcher = pattern.matcher(eventHtml);
+		if (matcher.find()) {
+			final Event e = new Event();
+			final String title = matcher.group(1);
+			final String name = StringEscapeUtils.unescapeHtml3(title);
+			e.setName(name);
+			System.out.println(e);
+			return e;
 		}
 		return null;
 	}
 
-	private List<Event> eventsFrom(final String html) {
-		final List<Event> el = new ArrayList<>();
-		final String eventInProgressPattern = "class=\"hoje-titulo\">(.*?)</a>";
-		final Pattern pattern = Pattern.compile(eventInProgressPattern, Pattern.DOTALL);
-		final Matcher matcher = pattern.matcher(html);
-		while (matcher.find()) {
-			final Event event = new Event();
-			event.setName(unescape(matcher.group(1)));
-			el.add(event);
-		}
-		return el;
-	}
-
-	private String unescape(final String word) {
-		return StringEscapeUtils.unescapeHtml3(word);
-	}
-
 	public static void main(final String[] args) {
-		final String string = "&#8220;";
-		final String unescape = StringEscapeUtils.unescapeHtml3(string);
-		System.out.println(unescape);
+		final RestfullHttpClient restfullHttpClient = new RestfullHttpClientImpl();
+		final EventService es = new UspEventService(restfullHttpClient);
+		final List<Event> et = es.retrieve();
+		System.out.println(et);
 	}
 
 }
